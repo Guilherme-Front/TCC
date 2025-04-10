5eb060c78027ff2ae8a4fa50bfea2b07b0167d7d
Guizinho99 <guilhermefariasg13@gmail.com>
2025-04-10 14:15:45 -0300

    full_Responsivo_Produtos/Login

--- js/scriptPerfil.js
+++ js/scriptPerfil.js
@@ -16,3 +16,24 @@ function selectLink() {
 }
 
 menuItem.forEach((item) => item.addEventListener('click', selectLink));
+
+const menu = document.querySelector(".menu-lateral");
+const body = document.body;
+
+if (window.innerWidth <= 400) {
+  // Clica no ícone do menu para abrir
+  menu.addEventListener("click", (e) => {
+    // Impede o clique de propagar para o body
+    e.stopPropagation();
+    body.classList.add("menu-expandido");
+  });
+
+  // Clica em qualquer lugar fora do menu = fecha
+  document.addEventListener("click", () => {
+    body.classList.remove("menu-expandido");
+  });
+
+  // Impede que cliques dentro do menu fechem ele
+  menu.addEventListener("click", (e) => e.stopPropagation());
+} 
+
